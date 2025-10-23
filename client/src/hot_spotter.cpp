3b788620e4691c3b11ef4c640fb5146f272d28dd
Exeos <69673405+Exeos@users.noreply.github.com>
2025-10-23 11:25:07 +0200

    rollback test commit.
    ssh works now :)

--- client/src/hot_spotter.cpp
+++ client/src/hot_spotter.cpp
@@ -18,64 +18,64 @@
 #endif
 
 namespace hot_spotter {
-  JavaVM *jvm = nullptr;
-  JNIEnv *jniEnv = nullptr;
-  jvmtiEnv *jvmTi = nullptr;
-  class_map_t classes = {};
+JavaVM *jvm = nullptr;
+JNIEnv *jniEnv = nullptr;
+jvmtiEnv *jvmTi = nullptr;
+class_map_t classes = {};
 
-  void init() {
-    if (!logger::InitConsole()) {
-      return;
-    }
-    logger::Log("Initializing");
+void init() {
+  if (!logger::InitConsole()) {
+    return;
+  }
+  logger::Log("Initializing");
 
-    Attacher *attacher = createAttacher();
-    if (!attacher->attach(jvm, jniEnv, jvmTi)) {
-      logger::Log("Failed to attach to jvm.");
-      tidy(); // Clean up before exiting
-      return;
-    }
-    delete attacher;
+  Attacher *attacher = createAttacher();
+  if (!attacher->attach(jvm, jniEnv, jvmTi)) {
+    logger::Log("Failed to attach to jvm.");
+    tidy(); // Clean up before exiting
+    return;
+  }
+  delete attacher;
 
-    if (!capabilities::setCapabilities()) {
-      logger::Log("Failed to get or set Capabilities");
-    }
-
-    if (!hooks::initHooks()) {
-      logger::Log("Failed to init hooks");
-    }
-
-    if (!class_dumper::dump()) {
-      logger::Log("Failed to setup class dump");
-    }
-
-    logger::Log("Initialized, starting gui");
-
-    startGui();
-
-    logger::Log("Exiting. DELETE ME");
-
-    tidy();
+  if (!capabilities::setCapabilities()) {
+    logger::Log("Failed to get or set Capabilities");
   }
 
-  void startGui() {
-    if (gui::init()) {
-      gui::render();
-      gui::cleanup();
-    }
+  if (!hooks::initHooks()) {
+    logger::Log("Failed to init hooks");
   }
 
-  void tidy() {
-    if (!hooks::removeHooks()) {
-      logger::Log("Failed to remove hooks");
-    }
-
-    for (const auto &[jclazz, clazzData]: classes | std::views::values) {
-      // delete manually allocated class file data memory
-      jniEnv->DeleteGlobalRef(jclazz);
-      delete[] clazzData.second;
-    }
-
-    logger::CloseConsole();
+  if (!class_dumper::dump()) {
+    logger::Log("Failed to setup class dump");
   }
+
+  logger::Log("Initialized, starting gui");
+
+  startGui();
+
+  logger::Log("Exiting.");
+
+  tidy();
+}
+
+void startGui() {
+  if (gui::init()) {
+    gui::render();
+    gui::cleanup();
+  }
+}
+
+void tidy() {
+  if (!hooks::removeHooks()) {
+    logger::Log("Failed to remove hooks");
+  }
+
+  for (const auto &[jclazz, clazzData] : classes | std::views::values) {
+    // delete manually allocated class file data memory
+    jniEnv->DeleteGlobalRef(jclazz);
+    delete[] clazzData.second;
+  }
+
+  logger::CloseConsole();
+}
 } // namespace hot_spotter
